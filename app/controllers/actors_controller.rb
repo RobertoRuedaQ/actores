8b5481d539b91886c2e0a5325efb3cab3cccfb97
roberto <rueda.roberto89@gmail.com>
2017-04-20 18:31:54 -0500

    first commit

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,19 @@ class ActorsController < ApplicationController
   def index
   @actors = Actor.all	
   end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+  @actor = Actor.new(actors_params)
+  @actor.save
+  redirect_to actors_path
+  end
+
+  private
+  def actors_params
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place,:image_url,:alive, :death_date,:death_place)
+  end
 end
